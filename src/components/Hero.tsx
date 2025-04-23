2d87cd2e635ca2048f1bca752bb193a9ff420149
Guillermo Alejandro Longueira Lopez <guillermolongueira@soliss.com>
2025-04-23 14:13:15 +0200

    cambios del hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,22 +9,20 @@ const Hero = () => {
         transition={{ duration: 1 }}
         className="text-5xl md:text-6xl font-extrabold mb-6"
       >
-        Diseño web moderno para negocios locales
+        Soluciones web modernas para impulsar tu presencia digital
       </motion.h1>
 
       <motion.p
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.5, duration: 1 }}
-        className="text-lg md:text-xl text-slate-300 mb-4"
+        className="text-lg md:text-xl text-slate-100 mb-4"
       >
-        Páginas rápidas, profesionales y optimizadas para captar clientes.
+        Desde portafolios creativos hasta blogs, landing pages y pequeñas aplicaciones.
       </motion.p>
 
-      <p className="text-slate-400 text-base max-w-xl mx-auto mb-8">
-       Desarrollador freelance con enfoque en diseño moderno y
-        rendimiento. Ayudo a negocios locales a destacar online con sitios que
-        ayuden mejorar potencialmente sus negocios dentro del mundo digital.
+      <p className="text-slate-300 text-xl max-w-xl mx-auto mb-8">
+        Soy desarrollador freelance enfocado en diseño moderno, funcionalidad y rendimiento. Creo sitios y apps personalizadas que ayudan a destacar en el mundo digital, ya seas emprendedor, creador de contenido o empresa.
       </p>
 
       <motion.a
@@ -33,7 +31,7 @@ const Hero = () => {
         whileTap={{ scale: 0.95 }}
         className="inline-block bg-blue-600 px-8 py-4 rounded-full shadow-lg hover:bg-blue-700 transition"
       >
-        Quiero mi web
+        Empezar mi proyecto
       </motion.a>
     </section>
   );
